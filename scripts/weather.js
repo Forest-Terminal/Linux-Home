01d6ff941a61a2b23c7ca01293c43f9c17df797c
Forest Terminal <138257699+Forest-Terminal@users.noreply.github.com>
2025-06-29 16:27:35 -0500

    Update weather.js
    Set the weather to hide my location by default

--- scripts/weather.js
+++ scripts/weather.js
@@ -5,7 +5,7 @@
  * You should have received a copy of the GNU General Public License along with this program.
  * If not, see <https://www.gnu.org/licenses/>.
  */
-
+localStorage.setItem("locationHidden", true)
 document.addEventListener("DOMContentLoaded", function () {
     const hideWeather = document.getElementById("hideWeather");
     const hideWeatherCheckbox = document.getElementById("hideWeatherCheckbox");
